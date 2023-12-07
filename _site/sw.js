c4fc9949dd4832b79b6a6f7bcb04e804770253df
daeyoung-k <eodud4976@gmail.com>
2023-12-07 09:14:50 +0900

    blog 준비

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701839147';
+const cacheName = 'chirpy-1701907690';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
