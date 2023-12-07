df186b1756b40fee0897dfca329a848495be073a
daeyoung-k <eodud4976@gmail.com>
2023-12-07 09:23:27 +0900

    배포 시작

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701907690';
+const cacheName = 'chirpy-1701908560';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
