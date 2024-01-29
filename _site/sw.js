bb4e119dec82ab659b1c8598e48a0381ee41e90f
daeyoung-k <eodud4976@gmail.com>
2024-01-29 14:23:23 +0900

    footer 제거

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706503362';
+const cacheName = 'chirpy-1706505737';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
