a73ad937e794b39429ddbe8fbf26f03485ebd9f8
daeyoung-k <eodud4976@gmail.com>
2024-01-29 13:44:08 +0900

    url 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706503126';
+const cacheName = 'chirpy-1706503362';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
